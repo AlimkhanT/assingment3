3a90953ff555cd1ec3df60875a07aa6c09a80d0d
AlimkhanT <alimkhantakhir@mail.ru>
2023-05-25 13:11:59 +0600

    assin3

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    // Animation for each testimonial block
+    $("#testimonial1").addClass("animated-card");
+    $("#testimonial2").addClass("animated-card");
+    $("#testimonial3").addClass("animated-card");
+    $("#testimonial4").addClass("animated-card");
+  });
+  
+
+  function validateForm() {
+    var name = document.getElementById("formGroupExampleInput").value;
+    var phoneNumber = document.getElementById("formGroupExampleInput2").value;
+    var description = document.getElementById("formGroupExampleInput3").value;
+
+    if (name === "") {
+      alert("Please enter your name.");
+      return false;
+    }
+
+    if (phoneNumber === "") {
+      alert("Please enter your phone number.");
+      return false;
+    }
+
+    if (description === "") {
+      alert("Please describe your case.");
+      return false;
+    }
+
+    return true;
+  }
+  function redirectToPaymentPage() {
+    window.location.href = "payment.html";
+  }
+
+
+
+  
+  var audio = document.getElementById("mySong");
+  var playButton = document.getElementById("play");
+  
+  playButton.onclick = function toggleAudio() {
+    if (audio.paused) {
+audio.play();
+} else {
+audio.pause();
+}
+  };
+
